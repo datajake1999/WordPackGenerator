f65a992346fa2d55a93adeccb695b4c6929f702e
datajake1999 <datajake@gmail.com>
2022-05-17 10:36:50 -0700

    added code

---
+++ 16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	FILE *fp;
+	unsigned int i;
+	printf("Generating your 16 bit word pack...\n");
+	for (i = 0; i < 65536; i++)
+	{
+		unsigned short num = i;
+		char filename[32];
+		sprintf(filename, "%d", i);
+		fp = fopen(filename, "wb");
+		fwrite(&num, 2, 1, fp);
+		fclose(fp);
+	}
+	printf("Done!\n");
+	return 0;
+}
---
+++ 8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	FILE *fp;
+	unsigned int i;
+	printf("Generating your 8 bit word pack...\n");
+	for (i = 0; i < 256; i++)
+	{
+		unsigned char num = i;
+		char filename[32];
+		sprintf(filename, "%d", i);
+		fp = fopen(filename, "wb");
+		fwrite(&num, 1, 1, fp);
+		fclose(fp);
+	}
+	printf("Done!\n");
+	return 0;
+}
