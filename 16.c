b9318bedbfbe05ffe9e2f8429d278841859eb6eb
datajake1999 <datajake@gmail.com>
2022-05-17 10:45:14 -0700

    Removed indents because that Is how you write pro level code.

--- 16.c
+++ 16.c
@@ -2,18 +2,18 @@
 
 int main()
 {
-	FILE *fp;
-	unsigned int i;
-	printf("Generating your 16 bit word pack...\n");
-	for (i = 0; i < 65536; i++)
-	{
-		unsigned short num = i;
-		char filename[32];
-		sprintf(filename, "%d", i);
-		fp = fopen(filename, "wb");
-		fwrite(&num, 2, 1, fp);
-		fclose(fp);
-	}
-	printf("Done!\n");
-	return 0;
+FILE *fp;
+unsigned int i;
+printf("Generating your 16 bit word pack...\n");
+for (i = 0; i < 65536; i++)
+{
+unsigned short num = i;
+char filename[32];
+sprintf(filename, "%d", i);
+fp = fopen(filename, "wb");
+fwrite(&num, 2, 1, fp);
+fclose(fp);
+}
+printf("Done!\n");
+return 0;
 }
--- 8.c
+++ 8.c
@@ -2,18 +2,18 @@
 
 int main()
 {
-	FILE *fp;
-	unsigned int i;
-	printf("Generating your 8 bit word pack...\n");
-	for (i = 0; i < 256; i++)
-	{
-		unsigned char num = i;
-		char filename[32];
-		sprintf(filename, "%d", i);
-		fp = fopen(filename, "wb");
-		fwrite(&num, 1, 1, fp);
-		fclose(fp);
-	}
-	printf("Done!\n");
-	return 0;
+FILE *fp;
+unsigned int i;
+printf("Generating your 8 bit word pack...\n");
+for (i = 0; i < 256; i++)
+{
+unsigned char num = i;
+char filename[32];
+sprintf(filename, "%d", i);
+fp = fopen(filename, "wb");
+fwrite(&num, 1, 1, fp);
+fclose(fp);
+}
+printf("Done!\n");
+return 0;
 }
